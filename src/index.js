5002a24000a4d00b2769c58616850ed13c276818
SprngVel <kraevvalentin98@gmail.com>
2024-07-08 16:09:49 +1000

    switch tab logic added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import 'normalize.css';
 import './style.css';
-import { renderAboutSection } from './about';
+import {renderAboutSection, createFooter} from './about.js';
+import {renderHomePage} from './homepage.js';
 
-renderAboutSection();
-console.log("I'm working");
+const content = document.querySelector('#content');
+const [home, menu, about, contact] = document.querySelectorAll('.nav-btn');
+
+(function initCreatePage() {
+    renderHomePage();
+    createFooter();
+})();
+
+home.addEventListener('click', () => {
+    content.innerHTML = '';
+    renderHomePage();
+}); 
+
+about.addEventListener('click', () => {
+    content.innerHTML = '';
+    renderAboutSection();
+});
